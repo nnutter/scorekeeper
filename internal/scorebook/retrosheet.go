2cef1fcdd72d3a6bcb5b1b6ec8acd9382f0d7a1d
Nathan Nutter <nnutter@duck.com>
2026-04-04 14:29:16 -0500

    Customize the order/layout of the keyboard buttons

--- internal/scorebook/retrosheet.go
+++ internal/scorebook/retrosheet.go
@@ -1,24 +1,35 @@
 package scorebook
 
 var PitchTokenRows = [][]string{
-	{"+", "*", ".", "1", "2", "3", ">"},
-	{"A", "B", "C", "F", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "X", "Y"},
+	[]string{"B", "C", "S", "K", "F", "L", "T"},
+	[]string{"1", "2", "3", ">", "H", "I", "A"},
+	{"+", "*", ".", "M", "N", "O", "P"},
+	{"Q", "R", "U", "V", "X", "Y"},
 }
 
 var BatterTokenRows = [][]string{
-	{"S", "D", "T", "HR", "DGR", "K", "W", "IW", "HP", "E", "FC", "FLE", "NP"},
-	{"SF", "SH", "GDP", "LDP", "FO", "G", "L", "P", "F", "1", "2", "3", "4", "5"},
-	{"6", "7", "8", "9", "(", ")", ".", ";", "/"},
+	[]string{"K", "S", "D", "T", "HR", "DGR"},
+	[]string{"W", "IW", "HP", "E", "FC", "FLE"},
+	[]string{"(", ")", "1", "2", "3", "4", "5"},
+	{"6", "7", "8", "9", ".", ";", "/"},
+	{"SF", "SH", "GDP", "LDP", "FO"},
+	[]string{"G", "L", "P", "F"},
 }
 
 var AdvanceTokenRows = [][]string{
-	{"B-1", "B-2", "B-3", "B-H", "1-2", "1-3", "1-H", "2-2", "2-3", "2-H", "3-3", "3-H"},
-	{"1X2", "1X3", "1XH", "2X3", "2XH", "3XH", ";", "(", ")", "E", "TH", "UR", "RBI", "NR"},
+	{"B-1", "B-2", "B-3", "B-H"},
+	[]string{"1-2", "1-3", "1-H", "1X2", "1X3", "1XH"},
+	[]string{"2-3", "2-H", "2X3", "2XH"},
+	[]string{"3-H", "3XH"},
+	{";", "(", ")", "E", "TH", "UR", "RBI", "NR"},
 }
 
 var RunnerTokenRows = [][]string{
-	{"SB2", "SB3", "SBH", "CS2", "CS3", "CSH", "PO1", "PO2", "PO3", "POCS2", "POCS3", "POCSH"},
-	{"WP", "PB", "BK", "DI", "OA", ".", ";", "(", ")"},
+	{"SB2", "SB3", "SBH", "WP"},
+	[]string{"CS2", "CS3", "CSH", "BK"},
+	[]string{"PO1", "PO2", "PO3", "DI"},
+	[]string{"POCS2", "POCS3", "POCSH", "OA"},
+	{"PB", ".", ";", "(", ")"},
 }
 
 const pitchChars = "+*.123>ABCFHIKLMNOPQRSTUVWXYZ"
