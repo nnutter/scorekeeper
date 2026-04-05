e2196fd1583719ee9684d11fc2505260ce316533
Nathan Nutter <nnutter@duck.com>
2026-04-04 21:52:10 -0500

    Update Pitches button order

--- internal/scorebook/retrosheet.go
+++ internal/scorebook/retrosheet.go
@@ -1,34 +1,35 @@
 package scorebook
 
 var PitchTokenRows = [][]string{
-	[]string{"B", "C", "S", "K", "F", "L", "T"},
-	[]string{"1", "2", "3", ">", "H", "I", "A"},
-	{"+", "*", ".", "M", "N", "O", "P"},
-	{"Q", "R", "U", "V", "X", "Y"},
+	{"B", "C", "S", "K", "F", "X"},
+	{"1", "2", "3", "A", "H", "I"},
+	{">", "L", "T"},
+	{"+", "*", ".", "M", "N", "O"},
+	{"Q", "R", "U", "V", "Y", "P"},
 }
 
 var BatterTokenRows = [][]string{
-	[]string{"K", "S", "D", "T", "HR", "DGR"},
-	[]string{"W", "IW", "HP", "E", "FC", "FLE"},
-	[]string{"(", ")", "1", "2", "3", "4", "5"},
+	{"K", "S", "D", "T", "HR", "DGR"},
+	{"W", "IW", "HP", "E", "FC", "FLE"},
+	{"(", ")", "1", "2", "3", "4", "5"},
 	{"6", "7", "8", "9", ".", ";", "/"},
 	{"SF", "SH", "GDP", "LDP", "FO"},
-	[]string{"G", "L", "P", "F"},
+	{"G", "L", "P", "F"},
 }
 
 var AdvanceTokenRows = [][]string{
 	{"B-1", "B-2", "B-3", "B-H"},
-	[]string{"1-2", "1-3", "1-H", "1X2", "1X3", "1XH"},
-	[]string{"2-3", "2-H", "2X3", "2XH"},
-	[]string{"3-H", "3XH"},
+	{"1-2", "1-3", "1-H", "1X2", "1X3", "1XH"},
+	{"2-3", "2-H", "2X3", "2XH"},
+	{"3-H", "3XH"},
 	{";", "(", ")", "E", "TH", "UR", "RBI", "NR"},
 }
 
 var RunnerTokenRows = [][]string{
 	{"SB2", "SB3", "SBH", "WP"},
-	[]string{"CS2", "CS3", "CSH", "BK"},
-	[]string{"PO1", "PO2", "PO3", "DI"},
-	[]string{"POCS2", "POCS3", "POCSH", "OA"},
+	{"CS2", "CS3", "CSH", "BK"},
+	{"PO1", "PO2", "PO3", "DI"},
+	{"POCS2", "POCS3", "POCSH", "OA"},
 	{"PB", ".", ";", "(", ")"},
 }
 
